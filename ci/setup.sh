2610b42b1c66a0d86a3cb93bf34f651a85e6ae43
Luís Leite <luis@leite.xyz>
2019-11-20 18:54:31 +0000

    Add Appveyor CI config and CI scripts

---
+++ ci/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# install and use the current node version set in the environment variable
+nvm install $CURRENT_NODE_VERSION
+nvm use $CURRENT_NODE_VERSION
+
+# update npm to latest version
+npm -g i npm
+
+# print tools versions
+echo 'NodeJS version:'
+node --version
+
+echo 'npm version:'
+npm --version
+
+# install dependencies
+npm ci
